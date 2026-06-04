e4806be23c34aa8b8654832ec8fccdadbacbb8d2
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-04 03:29:40 -0400

    Moved ShortestPathController.java to controller folder

--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/controller/ShortestPathController.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/controller/ShortestPathController.java
@@ -1,4 +1,4 @@
-package com.example.shortesttm_path.apis;
+package com.example.shortesttm_path.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
