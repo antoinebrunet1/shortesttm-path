d18811e2b00714810b23ba16e0779f63c401912b
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-04 14:31:16 -0400

    Used ShortestPathService in ShortestPathController

--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/controller/ShortestPathController.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/controller/ShortestPathController.java
@@ -1,12 +1,21 @@
 package com.example.shortesttm_path.controller;
 
+import com.example.shortesttm_path.service.ShortestPathService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShortestPathController {
+    private final ShortestPathService shortestPathService;
+
+    @Autowired
+    public ShortestPathController(ShortestPathService shortestPathService) {
+        this.shortestPathService = shortestPathService;
+    }
+
     @GetMapping("/greet")
     public String greetTheUser() {
-        return "Hello User";
+        return shortestPathService.getGreeting();
     }
 }
