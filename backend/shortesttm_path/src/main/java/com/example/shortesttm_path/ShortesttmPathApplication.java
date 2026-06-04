ed7057f25de597a38818df91986ee57204d10fa6
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-03 22:10:22 -0400

    Setup Spring Boot

---
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/ShortesttmPathApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortesttm_path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortesttmPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortesttmPathApplication.class, args);
+	}
+
+}
---
+++ backend/shortesttm_path/src/test/java/com/example/shortesttm_path/ShortesttmPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortesttm_path;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortesttmPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
