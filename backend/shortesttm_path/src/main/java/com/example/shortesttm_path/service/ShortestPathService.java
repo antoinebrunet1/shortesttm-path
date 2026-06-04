b1f8b9ad8d70039aaed9eb1d680977a59771e4f8
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-04 03:28:26 -0400

    Created ShortestPathService.java with a test method

---
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/service/ShortestPathService.java
@@ -0,0 +1,10 @@
+package com.example.shortesttm_path.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShortestPathService {
+    public String getGreeting() {
+        return "Hello";
+    }
+}
