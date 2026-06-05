665cf1e340ce240db125fff6bf75d96c2b6b357b
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-05 16:13:55 -0400

    Removed unused imports

--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/controller/ShortestPathController.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/controller/ShortestPathController.java
@@ -1,7 +1,5 @@
 package com.example.shortesttm_path.controller;
 
-import com.example.shortesttm_path.service.ShortestPathService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
