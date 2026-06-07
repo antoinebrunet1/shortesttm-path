b858c6f61d8e1971ae2e42d01442ce35540f4a86
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-07 15:24:07 -0400

    Took care of warnings in all Java files

--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/ShortesttmPathApplication.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/ShortesttmPathApplication.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 @SpringBootApplication
 public class ShortesttmPathApplication {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		SpringApplication.run(ShortesttmPathApplication.class, args);
 	}
 
--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/security/DevSecurityConfig.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/security/DevSecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class DevSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) {
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
