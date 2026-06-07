d66e805645120896893e15bcdcba3d77aed22931
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-07 15:21:31 -0400

    Removed unused imports in ShortesttmPathApplication

--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/ShortesttmPathApplication.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/ShortesttmPathApplication.java
@@ -1,11 +1,9 @@
 package com.example.shortesttm_path;
 
-import com.example.shortesttm_path.util.ShortestPathUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
-import java.util.List;
 
 @SpringBootApplication
 public class ShortesttmPathApplication {
