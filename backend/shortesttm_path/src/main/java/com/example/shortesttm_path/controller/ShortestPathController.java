f6163f27ee948398053a25d902e782230a6efffa
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-07 15:11:26 -0400

    Added handle method in controller

--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/controller/ShortestPathController.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/controller/ShortestPathController.java
@@ -1,18 +1,21 @@
 package com.example.shortesttm_path.controller;
 
 import com.example.shortesttm_path.data.ShortestPathBean;
+import com.example.shortesttm_path.exception.StationsOnSameLineException;
 import com.example.shortesttm_path.util.ShortestPathUtil;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/shortest_path")
 public class ShortestPathController {
+    @ExceptionHandler(StationsOnSameLineException.class)
+    public ResponseEntity<String> handle() {
+        return ResponseEntity.badRequest().body("Provided stations are on the same line");
+    }
+
     @GetMapping()
     public ResponseEntity<ShortestPathBean> getShortestPath(@RequestParam String startingStation,
                                                             @RequestParam String destinationStation) {
