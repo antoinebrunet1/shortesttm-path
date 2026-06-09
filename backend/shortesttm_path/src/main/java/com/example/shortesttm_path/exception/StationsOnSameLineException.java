2be590c3a6fbf93da24634c9bc3c0e3b6c22a362
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-09 18:58:02 -0400

    Added Javadoc in StationsOnSameLineException

--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/exception/StationsOnSameLineException.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/exception/StationsOnSameLineException.java
@@ -1,6 +1,13 @@
 package com.example.shortesttm_path.exception;
 
+/**
+ * Is thrown when the two stations are on the same line. This includes the same station given twice and neighbor
+ * stations.
+ */
 public class StationsOnSameLineException extends RuntimeException {
+    /**
+     * Standard RuntimeException constructor with a custom message.
+     */
     public StationsOnSameLineException() {
         super("Provided stations are on the same line");
     }
