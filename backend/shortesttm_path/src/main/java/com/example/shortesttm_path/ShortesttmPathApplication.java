8bab2b2442fd4ba92275fcbf50504f1bfe31ce45
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-09 19:19:09 -0400

    Added Javadoc in ShortesttmPathApplication

--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/ShortesttmPathApplication.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/ShortesttmPathApplication.java
@@ -3,9 +3,21 @@ package com.example.shortesttm_path;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class of the Spring Boot application.
+ */
 @SpringBootApplication
 public class ShortesttmPathApplication {
+	/**
+	 * The default constructor.
+	 */
+	public ShortesttmPathApplication() {
+	}
 
+	/**
+	 * The main method of the Spring Boot application.
+	 * @param args The arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ShortesttmPathApplication.class, args);
 	}
