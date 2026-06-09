274320316f29db87dea4f86ebe34c123063bfb5d
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-09 19:01:07 -0400

    Added Javadoc to DevSecurityConfig

--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/security/DevSecurityConfig.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/security/DevSecurityConfig.java
@@ -7,10 +7,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * The Spring Security configuration class for the dev profile used in development.
+ */
 @Configuration
 @Profile("dev")
 public class DevSecurityConfig {
+    /**
+     * The default constructor.
+     */
+    public DevSecurityConfig() {
+    }
 
+    /**
+     * Disables Spring Security in dev.
+     * @param http The HttpSecurity.
+     * @return The SecurityFilterChain.
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
