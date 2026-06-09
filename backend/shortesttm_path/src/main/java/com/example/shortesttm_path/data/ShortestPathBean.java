bcaeab9f768a40bdcc47a14fe1c0fb6a2896877c
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-09 18:52:40 -0400

    Added Javadoc to ShortestPathBean

--- backend/shortesttm_path/src/main/java/com/example/shortesttm_path/data/ShortestPathBean.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttm_path/data/ShortestPathBean.java
@@ -4,8 +4,17 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * Represents the shortest metro path (the one with the fewer stations) between two STM metro stations.
+ */
 @Data
 public class ShortestPathBean {
+    /**
+     * The default constructor.
+     */
+    public ShortestPathBean() {
+    }
+
     String startingStation;
     String destinationStation;
     List<String> stationsToSwitchLines;
