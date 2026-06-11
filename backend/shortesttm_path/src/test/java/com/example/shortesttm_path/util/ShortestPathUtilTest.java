ccdb90408a28aae261ed736145197a61c8ec4052
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-11 18:32:45 -0400

    Removed the Assertions. in ShortestPathUtilTest

--- backend/shortesttm_path/src/test/java/com/example/shortesttm_path/util/ShortestPathUtilTest.java
+++ backend/shortesttm_path/src/test/java/com/example/shortesttm_path/util/ShortestPathUtilTest.java
@@ -2,10 +2,12 @@ package com.example.shortesttm_path.util;
 
 import com.example.shortesttm_path.data.ShortestPathBean;
 import com.example.shortesttm_path.exception.StationsOnSameLineException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ShortestPathUtilTest {
     @Test
@@ -15,14 +17,14 @@ public class ShortestPathUtilTest {
         List<String> expectedStationsToSwitchLines = List.of("Berri-UQAM");
         ShortestPathBean actualPath = ShortestPathUtil.getShortestPath(inputStartingStation, inputDestinationStation);
 
-        Assertions.assertEquals(inputStartingStation, actualPath.getStartingStation());
-        Assertions.assertEquals(inputDestinationStation, actualPath.getDestinationStation());
-        Assertions.assertEquals(expectedStationsToSwitchLines, actualPath.getStationsToSwitchLines());
+        assertEquals(inputStartingStation, actualPath.getStartingStation());
+        assertEquals(inputDestinationStation, actualPath.getDestinationStation());
+        assertEquals(expectedStationsToSwitchLines, actualPath.getStationsToSwitchLines());
     }
 
     @Test
     public void getShortestPathSameLineShouldThrowStationsOnSameLineException() {
-        Assertions.assertThrows(StationsOnSameLineException.class, () -> {
+        assertThrows(StationsOnSameLineException.class, () -> {
             ShortestPathUtil.getShortestPath("McGill", "Viau");
         });
     }
