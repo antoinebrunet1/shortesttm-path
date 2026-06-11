e94e3dcb4a501b1de0bef6399582b35612589cad
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-11 17:56:09 -0400

    Added ShortestPathUtilTest class and getShortestPathHappyPath

---
+++ backend/shortesttm_path/src/test/java/com/example/shortesttm_path/util/ShortestPathUtilTest.java
@@ -0,0 +1,23 @@
+package com.example.shortesttm_path.util;
+
+import com.example.shortesttm_path.data.ShortestPathBean;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ShortestPathUtilTest {
+    @Test
+    public void getShortestPathHappyPath() {
+        String inputStartingStation = "Laurier";
+        String inputDestinationStation = "Charlevoix";
+        List<String> expectedStationsToSwitchLines = List.of("Berri-UQAM");
+        ShortestPathBean actualPath = ShortestPathUtil.getShortestPath(
+                inputStartingStation,
+                inputDestinationStation);
+
+        Assertions.assertEquals(inputStartingStation, actualPath.getStartingStation());
+        Assertions.assertEquals(inputDestinationStation, actualPath.getDestinationStation());
+        Assertions.assertEquals(expectedStationsToSwitchLines, actualPath.getStationsToSwitchLines());
+    }
+}
