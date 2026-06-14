a5cfd69d0c9d9e73b096315bce7dc91ba2732a73
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-14 03:35:18 -0400

    Added API key authentication

--- backend/shortesttm_path/src/main/java/com/example/shortesttmpath/ShortesttmPathApplication.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttmpath/ShortesttmPathApplication.java
@@ -2,11 +2,13 @@ package com.example.shortesttmpath;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
 /**
  * The main class of the Spring Boot application.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class ShortesttmPathApplication {
   /**
    * The default constructor.
