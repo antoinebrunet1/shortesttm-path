6241248dedced519ad772a93673f27189855790f
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-14 03:48:05 -0400

    Put the API key in .env

--- backend/shortesttm_path/src/main/java/com/example/shortesttmpath/service/AuthenticationService.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttmpath/service/AuthenticationService.java
@@ -1,6 +1,7 @@
 package com.example.shortesttmpath.service;
 
 import com.example.shortesttmpath.authenticationtoken.ApiKeyAuthentication;
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
@@ -9,7 +10,7 @@ import org.springframework.security.core.authority.AuthorityUtils;
 public class AuthenticationService {
 
   private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
-  private static final String AUTH_TOKEN = "Baeldung";
+  private static final String AUTH_TOKEN = Dotenv.load().get("API_KEY");
 
   public static Authentication getAuthentication(HttpServletRequest request) {
     String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
