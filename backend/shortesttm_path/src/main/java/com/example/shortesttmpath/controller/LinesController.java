d8b35d7db5b48bfd520548340e84bc4aaaa50c5d
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-16 18:44:58 -0400

    Added getAllLines to LinesController.java

--- backend/shortesttm_path/src/main/java/com/example/shortesttmpath/controller/LinesController.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttmpath/controller/LinesController.java
@@ -1,5 +1,11 @@
 package com.example.shortesttmpath.controller;
 
+import com.example.shortesttmpath.util.LinesUtil;
+import java.util.List;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +19,18 @@ public class LinesController {
    * The default constructor.
    */
   public LinesController() {
+  }
+
+  /**
+   * Returns the name of each metro line.
+   *
+   * @return The name of each metro line.
+   */
+  @GetMapping()
+  public ResponseEntity<List<String>> getAllLines() {
+    HttpHeaders headers = new HttpHeaders();
+    List<String> allLines = LinesUtil.getAllLines();
+
+    return new ResponseEntity<>(allLines, headers, HttpStatus.OK);
   }
 }
