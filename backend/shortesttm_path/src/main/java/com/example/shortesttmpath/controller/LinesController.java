13abe30ed4b8e3d98cfe166aeee08cc2142db9c0
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-16 18:29:54 -0400

    Added LinesController.java

---
+++ backend/shortesttm_path/src/main/java/com/example/shortesttmpath/controller/LinesController.java
@@ -0,0 +1,17 @@
+package com.example.shortesttmpath.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The controller related to lines.
+ */
+@RestController
+@RequestMapping("/lines")
+public class LinesController {
+  /**
+   * The default constructor.
+   */
+  public LinesController() {
+  }
+}
