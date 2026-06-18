8071d39e6daaffa40848f1e80b7323ff6b62c22d
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-18 02:04:54 -0400

    Created blank starting-line-station component

---
+++ frontend/shortesttm-path/src/app/starting-line-station/starting-line-station.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starting-line-station',
+  imports: [],
+  templateUrl: './starting-line-station.html',
+  styleUrl: './starting-line-station.css',
+})
+export class StartingLineStation {
+
+}
