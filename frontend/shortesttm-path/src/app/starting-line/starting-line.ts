55338ed30193e8143fd12869493188609248348d
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-18 02:07:23 -0400

    Created blank starting-line component

---
+++ frontend/shortesttm-path/src/app/starting-line/starting-line.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starting-line',
+  imports: [],
+  templateUrl: './starting-line.html',
+  styleUrl: './starting-line.css',
+})
+export class StartingLine {
+
+}
