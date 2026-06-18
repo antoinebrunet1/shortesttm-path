b04409bc83c80b35bd3770d1d0bac876e03f03a1
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-18 01:41:53 -0400

    Created blank instructions component

---
+++ frontend/shortesttm-path/src/app/instructions/instructions.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructions',
+  imports: [],
+  templateUrl: './instructions.html',
+  styleUrl: './instructions.css',
+})
+export class Instructions {
+
+}
