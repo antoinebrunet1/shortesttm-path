f40d679c69f740ca6de84a29ba6bf22751f97988
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-19 19:50:23 -0400

    Added blank shortest-path component

---
+++ frontend/shortesttm-path/src/app/components/shortest-path/shortest-path.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShortestPath } from './shortest-path';
+
+describe('ShortestPath', () => {
+  let component: ShortestPath;
+  let fixture: ComponentFixture<ShortestPath>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ShortestPath]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ShortestPath);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/shortesttm-path/src/app/components/shortest-path/shortest-path.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shortest-path',
+  imports: [],
+  templateUrl: './shortest-path.html',
+  styleUrl: './shortest-path.css',
+})
+export class ShortestPath {
+
+}
