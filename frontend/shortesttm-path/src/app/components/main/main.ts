4a36b800c070b79123920b231328b9e3c22f3c06
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-19 18:14:01 -0400

    Fixed errors related to deletion of starting-line and starting-line-station components

--- frontend/shortesttm-path/src/app/components/main/main.ts
+++ frontend/shortesttm-path/src/app/components/main/main.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Instructions } from '../instructions/instructions';
-import { StartingLineStation } from '../starting-line-station/starting-line-station';
 import { LinesService } from '../../services/lines-service/lines-service';
 import { AsyncPipe } from '@angular/common';
 import { StationsService } from '../../services/stations-service/stations-service';
@@ -8,7 +7,7 @@ import { StartStation } from '../start-station/start-station';
 
 @Component({
   selector: 'app-main',
-  imports: [Instructions, StartingLineStation, AsyncPipe, StartStation],
+  imports: [Instructions, AsyncPipe, StartStation],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
