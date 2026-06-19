796177dccfd43dc233ca9962e9e0047533d82ef5
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-19 17:54:57 -0400

    Added allStations input in start-station.ts

--- frontend/shortesttm-path/src/app/components/main/main.ts
+++ frontend/shortesttm-path/src/app/components/main/main.ts
@@ -4,10 +4,11 @@ import { StartingLineStation } from '../starting-line-station/starting-line-stat
 import { LinesService } from '../../services/lines-service/lines-service';
 import { AsyncPipe } from '@angular/common';
 import { StationsService } from '../../services/stations-service/stations-service';
+import { StartStation } from '../start-station/start-station';
 
 @Component({
   selector: 'app-main',
-  imports: [Instructions, StartingLineStation, AsyncPipe],
+  imports: [Instructions, StartingLineStation, AsyncPipe, StartStation],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
--- frontend/shortesttm-path/src/app/components/start-station/start-station.ts
+++ frontend/shortesttm-path/src/app/components/start-station/start-station.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-start-station',
   imports: [],
   templateUrl: './start-station.html',
   styleUrl: './start-station.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StartStation {
-
+  @Input() allStations: any;
 }
