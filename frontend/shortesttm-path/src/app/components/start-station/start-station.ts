4edc0e7fd394262a539d1c59f019d1a8cde2d51e
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-19 19:04:35 -0400

    Put allStations in mat-select in start-station component

--- frontend/shortesttm-path/src/app/components/start-station/start-station.ts
+++ frontend/shortesttm-path/src/app/components/start-station/start-station.ts
@@ -1,8 +1,12 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-start-station',
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
   templateUrl: './start-station.html',
   styleUrl: './start-station.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
