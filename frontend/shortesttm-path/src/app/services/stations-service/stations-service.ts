68c88836fd91ddfae5defd7d809f2a993caf61c5
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-19 17:46:20 -0400

    Completed StationsService

--- frontend/shortesttm-path/src/app/services/stations-service/stations-service.ts
+++ frontend/shortesttm-path/src/app/services/stations-service/stations-service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StationsService {
-  
+  private http = inject(HttpClient);
+
+  getAllStations(): any {
+    const path = 'http://localhost:8080/stations';
+    const headers = new HttpHeaders().set('X-API-KEY', environment.apiKey);
+    return this.http.get<string[]>(path, { headers });
+  }
 }
