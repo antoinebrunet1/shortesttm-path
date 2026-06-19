498806d7fd688efa0561e34cc2549a96a5ac443e
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-19 14:14:36 -0400

    Completed LinesService

--- frontend/shortesttm-path/src/app/services/lines-service/lines-service.ts
+++ frontend/shortesttm-path/src/app/services/lines-service/lines-service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LinesService {
-  
+  private http = inject(HttpClient);
+
+  getAllLines(): any {
+    const path = 'http://localhost:8080/lines';
+    const headers = new HttpHeaders().set('X-API-KEY', environment.apiKey);
+    return this.http.get<string[]>(path, { headers });
+  }
 }
