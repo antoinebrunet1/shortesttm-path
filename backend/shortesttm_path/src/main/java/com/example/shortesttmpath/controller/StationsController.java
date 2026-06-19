07e993f4cd0ace23e0f325aff74e1fb6fcd5cffd
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-19 18:04:20 -0400

    Added StationsController with getAllStationsInAlphabeticalOrder

---
+++ backend/shortesttm_path/src/main/java/com/example/shortesttmpath/controller/StationsController.java
@@ -0,0 +1,23 @@
+package com.example.shortesttmpath.controller;
+
+import com.example.shortesttmpath.util.ShortestPathUtil;
+import java.util.List;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stations")
+public class StationsController {
+  @GetMapping("alphabetical-order")
+  public ResponseEntity<List<String>> getAllStationsInAlphabeticalOrder() {
+    HttpHeaders headers = new HttpHeaders();
+    List<String> allStationsInAlphabeticalOrder =
+        ShortestPathUtil.getAllStationsInAlphabeticalOrder();
+
+    return new ResponseEntity<>(allStationsInAlphabeticalOrder, headers, HttpStatus.OK);
+  }
+}
