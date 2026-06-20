4bd4b1d39196936b0917f825ab26e86296844cd0
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-20 03:26:15 -0400

    Completed api-key interceptor

--- frontend/shortesttm-path/src/app/interceptors/api-key/api-key-interceptor.ts
+++ frontend/shortesttm-path/src/app/interceptors/api-key/api-key-interceptor.ts
@@ -1,5 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const cloned = req.clone({
+    setHeaders: {
+      'X-API-KEY': environment.apiKey,
+    },
+  });
+
+  return next(cloned);
 };
