8f2584a60777c56a888306c1b164ed5c29a7ca95
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-20 03:03:05 -0400

    Created blank station component

---
+++ frontend/shortesttm-path/src/app/components/station/station.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-station',
+  imports: [],
+  templateUrl: './station.html',
+  styleUrl: './station.css',
+})
+export class Station {
+
+}
