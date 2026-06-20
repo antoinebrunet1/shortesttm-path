b16ce18dd1c4e6a5f0e53e4ca27ddaeee279e34f
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-19 20:10:58 -0400

    Added getShortestPath to ShortestPathService

--- frontend/shortesttm-path/src/app/services/shortest-path-service/shortest-path-service.ts
+++ frontend/shortesttm-path/src/app/services/shortest-path-service/shortest-path-service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShortestPathService {
-  
+  private http = inject(HttpClient);
+
+  getShortestPath(): any {
+    const path = 'http://localhost:8080/shortest_path';
+    const headers = new HttpHeaders().set('X-API-KEY', environment.apiKey);
+    return this.http.get<ShortestPath>(path, { headers });
+  }
 }
