7793c45021e2851eaa9e09fb0a651880acb9ae3a
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-20 02:52:15 -0400

    Fixed error in main.ts

--- frontend/shortesttm-path/src/app/components/main/main.ts
+++ frontend/shortesttm-path/src/app/components/main/main.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { Instructions } from '../instructions/instructions';
-import { LinesService } from '../../services/lines-service/lines-service';
 import { AsyncPipe } from '@angular/common';
 import { StationsService } from '../../services/stations-service/stations-service';
 import { StartStation } from '../start-station/start-station';
