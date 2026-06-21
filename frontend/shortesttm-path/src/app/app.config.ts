a0eb104deb6de83af18ca8ababad262ebe1ea5a7
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-20 21:18:19 -0400

    Fixed API key issue in frontend

--- frontend/shortesttm-path/src/app/app.config.ts
+++ frontend/shortesttm-path/src/app/app.config.ts
@@ -5,14 +5,15 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { apiKeyInterceptor } from './interceptors/api-key/api-key-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([apiKeyInterceptor])),
   ],
 };
