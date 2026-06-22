c2755c7e818bff9ee396f9ec93445980c59ef814
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-21 21:01:14 -0400

    Added clicking calculate button displays path

--- frontend/shortesttm-path/src/app/components/main/main.spec.ts
+++ frontend/shortesttm-path/src/app/components/main/main.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 import { ShortestPathService } from '../../services/shortest-path-service/shortest-path-service';
 import { StationsService } from '../../services/stations-service/stations-service';
 import { Main } from './main';
 
-describe('Main', () => {
+fdescribe('Main', () => {
   let component: Main;
   let fixture: ComponentFixture<Main>;
 
@@ -47,5 +48,16 @@ describe('Main', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('clicking calculate button displays path', () => {
+    const calculateButton = fixture.debugElement.query(By.css('button'));
+    calculateButton.triggerEventHandler('click');
+    fixture.detectChanges();
+    const expectedPath =
+      ' Start at Acadie station.  Go to Snowdon station. You will switch lines at that station.  Go to Lionel-Groulx station. You will switch lines at that station.  Go to Angrignon station. ';
+    expect(fixture.debugElement.query(By.css('app-shortest-path')).nativeNode.textContent).toBe(
+      expectedPath,
+    );
   });
 });
