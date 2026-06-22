18dabb34490c6c726849ae63b5d219975b8a48fc
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-21 20:37:29 -0400

    Made setup in main.spec.ts

--- frontend/shortesttm-path/src/app/components/main/main.spec.ts
+++ frontend/shortesttm-path/src/app/components/main/main.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { of } from 'rxjs';
+import { ShortestPathService } from '../../services/shortest-path-service/shortest-path-service';
+import { StationsService } from '../../services/stations-service/stations-service';
 import { Main } from './main';
 
 describe('Main', () => {
@@ -7,8 +10,34 @@ describe('Main', () => {
   let fixture: ComponentFixture<Main>;
 
   beforeEach(async () => {
+    const stationsServiceSpy = jasmine.createSpyObj<StationsService>([
+      'getAllStationsInAlphabeticalOrder',
+    ]);
+    stationsServiceSpy.getAllStationsInAlphabeticalOrder.and.callFake(function () {
+      return of(['Acadie', 'Angrignon']);
+    });
+    const shortestPathServiceSpy = jasmine.createSpyObj<ShortestPathService>(['getShortestPath']);
+    shortestPathServiceSpy.getShortestPath.and.callFake(
+      function (startingStation, destinationStation) {
+        return of({
+          startingStation: 'Acadie',
+          destinationStation: 'Angrignon',
+          stationsToSwitchLines: ['Snowdon', 'Lionel-Groulx'],
+        });
+      },
+    );
     await TestBed.configureTestingModule({
       imports: [Main],
+      providers: [
+        {
+          provide: StationsService,
+          useValue: stationsServiceSpy,
+        },
+        {
+          provide: ShortestPathService,
+          useValue: shortestPathServiceSpy,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Main);
