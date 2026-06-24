b7f6c86ca7bbe1b8674f584e59b4e09fa0da16b2
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-24 18:37:06 -0400

    Undid simplifications of the .sh files

--- wait_for_api.sh
+++ wait_for_api.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if curl -s --retry 60 --retry-delay 1 http://localhost:8080/actuator/health; then
-  echo "API is up"
-  exit 0
+for i in {1..59} ; do
+    if [ "$(curl -s http://localhost:8080/actuator/health)" ]; then
+        echo "API is up"
+        exit 0
+    fi
+    echo "Waiting for API..."
+    sleep 1
+done
+if [ "$(curl -s http://localhost:8080/actuator/health)" ]; then
+    echo "API is up"
+    exit 0
 fi
-
 echo "API failed to start"
 exit 1
--- wait_for_frontend.sh
+++ wait_for_frontend.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if curl -s --retry 60 --retry-delay 1 http://localhost:4200/; then
-  echo "Frontend is up"
-  exit 0
+for i in {1..59} ; do
+    if [ "$(curl -s http://localhost:4200/)" ]; then
+        echo "Frontend is up"
+        exit 0
+    fi
+    echo "Waiting for frontend..."
+    sleep 1
+done
+if [ "$(curl -s http://localhost:4200/)" ]; then
+    echo "Frontend is up"
+    exit 0
 fi
-
 echo "Frontend failed to start"
 exit 1
