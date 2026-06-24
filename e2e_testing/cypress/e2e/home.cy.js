c4dc823b33e22235c935306bf19955a68a87e19f
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-24 18:57:50 -0400

    Changed order of e2e tests

--- e2e_testing/cypress/e2e/home.cy.js
+++ e2e_testing/cypress/e2e/home.cy.js
@@ -3,19 +3,19 @@ describe("Home page tests", () => {
     cy.visit("http://localhost:4200/");
   });
 
-  it("oneTansfer", () => {
-    cy.happyPathTest("Laurier", ["Berri-UQAM"], "Charlevoix");
-  });
-
-  it("twoTansfers", () => {
-    cy.happyPathTest("Outremont", ["Snowdon", "Lionel-Groulx"], "Angrignon");
-  });
-
   it("sameMetroLine", () => {
     cy.clickFromHtmlTag("button");
     cy.get("mat-error").should(
       "have.text",
       "Provided stations are on the same line",
     );
+  });
+
+  it("oneTansfer", () => {
+    cy.happyPathTest("Laurier", ["Berri-UQAM"], "Charlevoix");
+  });
+
+  it("twoTansfers", () => {
+    cy.happyPathTest("Outremont", ["Snowdon", "Lionel-Groulx"], "Angrignon");
   });
 });
