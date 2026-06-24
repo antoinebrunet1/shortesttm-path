8c30d3d62425cdc8db87b810cf60915a3ac61eee
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-24 17:53:27 -0400

    Simplified wait_for_api.sh

--- wait_for_api.sh
+++ wait_for_api.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-for i in {1..59} ; do
-    if [ "$(curl -s http://localhost:8080/actuator/health)" ]; then
-        echo "API is up"
-        exit 0
-    fi
-    echo "Waiting for API..."
-    sleep 1
-done
-if [ "$(curl -s http://localhost:8080/actuator/health)" ]; then
-    echo "API is up"
-    exit 0
+if curl -sf --retry 60 --rety-delay 1 http://localhost:8080/actuator/health; then
+  echo "API is up"
+  exit 0
 fi
+
 echo "API failed to start"
 exit 1
