55a4da5d436fc880b2618e696191bad3606af4dd
Antoine Brunet <antoinebrunet1@outlook.com>
2026-06-30 22:39:54 -0400

    Added equals method to NonEndingStationInPath.java

--- backend/shortesttm_path/src/main/java/com/example/shortesttmpath/data/NonEndingStationInPath.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttmpath/data/NonEndingStationInPath.java
@@ -1,5 +1,6 @@
 package com.example.shortesttmpath.data;
 
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,4 +10,13 @@ public class NonEndingStationInPath {
   String name;
   String line;
   String direction;
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof NonEndingStationInPath that)) {
+      return false;
+    }
+    return Objects.equals(name, that.name) && Objects.equals(line, that.line) &&
+        Objects.equals(direction, that.direction);
+  }
 }
