28755c27a4b7685521ddc5af316a335bd2222adb
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-02 18:06:39 -0400

    Fixed all Javadoc warnings

--- backend/shortesttm_path/src/main/java/com/example/shortesttmpath/controller/UptimeController.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttmpath/controller/UptimeController.java
@@ -6,8 +6,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The controller to make sure the HEAD request from UptimeRobot does not return 404.
+ */
 @RestController
 public class UptimeController {
+  /**
+   * The default constructor.
+   */
+  public UptimeController() {
+  }
+
+  /**
+   * To make sure the HEAD request from UptimeRobot does not return 404.
+   *
+   * @return 200 OK with a success message.
+   */
   @GetMapping()
   public ResponseEntity<String> uptime() {
     HttpHeaders headers = new HttpHeaders();
