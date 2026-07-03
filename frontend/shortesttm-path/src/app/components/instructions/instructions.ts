0da62fe0cc9ab5b05e44c0a96fa5fc9e5d8258c0
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-02 21:20:32 -0400

    Added icon for metro map button

--- frontend/shortesttm-path/src/app/components/instructions/instructions.ts
+++ frontend/shortesttm-path/src/app/components/instructions/instructions.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-instructions',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './instructions.html',
   styleUrl: './instructions.css',
 })
