5a69301bb03b4a304342bcc6eadf7cfd0d146bb7
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-02 21:08:02 -0400

    Used Angular Material button for metro map link

--- frontend/shortesttm-path/src/app/components/instructions/instructions.ts
+++ frontend/shortesttm-path/src/app/components/instructions/instructions.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-instructions',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './instructions.html',
   styleUrl: './instructions.css',
 })
