2634ace1fe0b2b3ca07259c34c03c2d045e62916
Antoine Brunet <antoinebrunet1@outlook.com>
2026-08-09 18:37:43 -0400

    Added FileUtilTest with getLinesReturnsCorrectLinesThreeLines

---
+++ backend/shortesttm_path/src/test/java/com/example/shortesttmpath/util/FileUtilTest.java
@@ -0,0 +1,21 @@
+package com.example.shortesttmpath.util;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FileUtilTest {
+  @Test
+  public void getLinesReturnsCorrectLinesThreeLines() throws IOException {
+    String fileName = "file_for_a_file_util_unit_test.txt";
+    List<String> expectedResult = List.of(
+        "Testing",
+        "FileUtil",
+        "class"
+    );
+    List<String> actualResult = FileUtil.getLines(fileName);
+
+    Assertions.assertEquals(expectedResult, actualResult);
+  }
+}
