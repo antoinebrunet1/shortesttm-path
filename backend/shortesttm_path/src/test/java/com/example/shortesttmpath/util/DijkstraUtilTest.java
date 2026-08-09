435770ccfed5e7ef482f4cd5a0ecfdb0032b8a2d
Antoine Brunet <antoinebrunet1@outlook.com>
2026-08-08 21:06:39 -0400

    Added DijkstraUtilTest with dijkstraReturnsCorrectPathThreeNodes

---
+++ backend/shortesttm_path/src/test/java/com/example/shortesttmpath/util/DijkstraUtilTest.java
@@ -0,0 +1,43 @@
+package com.example.shortesttmpath.util;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DijkstraUtilTest {
+  /*
+  Graph:
+
+  1-1-2
+  \   |
+   4  2
+    \ |
+      3
+   */
+  @Test
+  public void dijkstraReturnsCorrectPathThreeNodes() {
+    List<int[]> node1 = List.of(
+        new int[] {2, 1},
+        new int[] {3, 4}
+    );
+    List<int[]> node2 = List.of(
+        new int[] {1, 1},
+        new int[] {3, 2}
+    );
+    List<int[]> node3 = List.of(
+        new int[] {2, 2},
+        new int[] {1, 4}
+    );
+    List<List<int[]>> graph = List.of(
+        node1,
+        node2,
+        node3
+    );
+    int start = 1;
+    int target = 3;
+    List<Integer> expectedResult = List.of(1, 2, 3);
+    List<Integer> actualResult = DijkstraUtil.dijkstra(graph, start, target);
+
+    Assertions.assertEquals(expectedResult, actualResult);
+  }
+}
