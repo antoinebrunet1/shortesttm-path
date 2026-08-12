915a1fa43e635ca91cbde384e14b994d2fea4141
Antoine Brunet <antoinebrunet1@outlook.com>
2026-08-12 15:54:07 -0400

    Used mocking in getShortestPathHappyPath

--- backend/shortesttm_path/src/test/java/com/example/shortesttmpath/util/ShortestPathUtilTest.java
+++ backend/shortesttm_path/src/test/java/com/example/shortesttmpath/util/ShortestPathUtilTest.java
@@ -6,34 +6,104 @@ import com.example.shortesttmpath.enums.Line;
 import com.example.shortesttmpath.exception.StationsNotValidException;
 import com.example.shortesttmpath.exception.StationsOnSameLineException;
 import java.io.IOException;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.hamcrest.MockitoHamcrest.argThat;
 
 public class ShortestPathUtilTest {
+  /*
+  Graph (Each vertex is bidirectional.):
+
+              D       E
+             / \     /
+           200 400 500
+           /     \ /
+  A--100--B--300--C
+
+  Same graph but with A = 0, B = 1, ... E = 4:
+
+              3       4
+             / \     /
+           200 400 500
+           /     \ /
+  0--100--1--300--2
+   */
     @Test
     public void getShortestPathHappyPath() throws IOException {
-        String inputStartingStation = "Laurier";
-        String inputDestinationStation = "Charlevoix";
-        ShortestPathBean actualPath = ShortestPathUtil.getInstance().getShortestPath(
-            inputStartingStation, inputDestinationStation);
-        ShortestPathBean expectedPath = new ShortestPathBean(
-                new NonEndingStationInPathBean(
-                    inputStartingStation,
-                    "ORANGE",
-                    "Côte-Vertu"
-                ),
-                inputDestinationStation,
-            List.of(new NonEndingStationInPathBean(
-                "Lionel-Groulx",
-                "GREEN",
-                "Angrignon"
-            )));
+      String startingStation = "A";
+      String destinationStation = "D";
+      List<String> blueLineStations = List.of("A", "B", "C");
+      List<String> greenLineStations = List.of("B", "D");
+      List<String> orangeLineStations = List.of("C", "D");
+      List<String> yellowLineStations = List.of("C", "E");
+      List<String> allStationsAlphabeticalOrder = List.of("A", "B", "C", "D", "E");
+      List<String> allStationsToSwitchLines = List.of("B", "C");
+      Map<String, Integer> stationsNamesToInts = Map.of(
+          "A", 0,
+          "B", 1,
+          "C", 2,
+          "D", 3,
+          "E", 4
+      );
+      Map<Integer, Map<Integer, Integer>> mapSrcToMapDestinationToDistanceInM = Map.of(
+          0, Map.of(
+              1, 100
+          ),
+          1, Map.of(
+              0, 100,
+              3, 200,
+              2, 300
+          ),
+          2, Map.of(
+              1, 300,
+              3, 400,
+              4, 500
+          ),
+          3, Map.of(
+              1, 200,
+              2, 400
+          ),
+          4, Map.of(
+              2, 500
+          )
+      );
+      List<Integer> path = List.of(0, 1, 3);
 
-        assertEquals(expectedPath, actualPath);
+        try (MockedStatic<FileUtil> fileUtilMocked = Mockito.mockStatic(FileUtil.class);
+             MockedStatic<DistancesUtil> distancesUtilMocked = Mockito.mockStatic(DistancesUtil.class);
+             MockedStatic<DijkstraUtil> dijkstraUtilMocked = Mockito.mockStatic(DijkstraUtil.class)) {
+          fileUtilMocked.when(() -> FileUtil.getLines("blue_line_stations.txt")).thenReturn(blueLineStations);
+          fileUtilMocked.when(() -> FileUtil.getLines("green_line_stations.txt")).thenReturn(greenLineStations);
+          fileUtilMocked.when(() -> FileUtil.getLines("orange_line_stations.txt")).thenReturn(orangeLineStations);
+          fileUtilMocked.when(() -> FileUtil.getLines("yellow_line_stations.txt")).thenReturn(yellowLineStations);
+          fileUtilMocked.when(() -> FileUtil.getLines("all_stations_alphabetical_order.txt")).thenReturn(allStationsAlphabeticalOrder);
+          fileUtilMocked.when(() -> FileUtil.getLines("all_stations_to_switch_lines.txt")).thenReturn(allStationsToSwitchLines);
+          distancesUtilMocked.when(() -> DistancesUtil.getMapScrToMapDestinationToDistanceInM(stationsNamesToInts)).thenReturn(mapSrcToMapDestinationToDistanceInM);
+          dijkstraUtilMocked.when(() -> DijkstraUtil.dijkstra(Mockito.any(), Mockito.eq(0), Mockito.eq(3))).thenReturn(path);
+          ShortestPathBean actualPath = ShortestPathUtil.getInstance().getShortestPath(
+              startingStation, destinationStation);
+          ShortestPathBean expectedPath = new ShortestPathBean(
+              new NonEndingStationInPathBean(
+                  startingStation,
+                  "BLUE",
+                  "C"
+              ),
+              destinationStation,
+              List.of(new NonEndingStationInPathBean(
+                  "B",
+                  "GREEN",
+                  "D"
+              )));
+
+          assertEquals(expectedPath, actualPath);
+        }
     }
 
     @Test
