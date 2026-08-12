21f1ae1258b60a2ef7e7fe5cd4a51219cd7292de
Antoine Brunet <antoinebrunet1@outlook.com>
2026-08-12 16:21:34 -0400

    Used getNewInstanceForUnitTest in ShortestPathUtilTest

--- backend/shortesttm_path/src/test/java/com/example/shortesttmpath/util/ShortestPathUtilTest.java
+++ backend/shortesttm_path/src/test/java/com/example/shortesttmpath/util/ShortestPathUtilTest.java
@@ -85,7 +85,7 @@ public class ShortestPathUtilTest {
           fileUtilMocked.when(() -> FileUtil.getLines("all_stations_to_switch_lines.txt")).thenReturn(allStationsToSwitchLines);
           distancesUtilMocked.when(() -> DistancesUtil.getMapScrToMapDestinationToDistanceInM(stationsNamesToInts)).thenReturn(mapSrcToMapDestinationToDistanceInM);
           dijkstraUtilMocked.when(() -> DijkstraUtil.dijkstra(Mockito.any(), Mockito.eq(0), Mockito.eq(3))).thenReturn(path);
-          ShortestPathBean actualPath = ShortestPathUtil.getInstance().getShortestPath(
+          ShortestPathBean actualPath = ShortestPathUtil.getNewInstanceForUnitTest().getShortestPath(
               startingStation, destinationStation);
           ShortestPathBean expectedPath = new ShortestPathBean(
               new NonEndingStationInPathBean(
@@ -107,24 +107,24 @@ public class ShortestPathUtilTest {
     @Test
     public void getShortestPathSameLineShouldThrowStationsOnSameLineException() {
         assertThrows(StationsOnSameLineException.class, () ->
-            ShortestPathUtil.getInstance().getShortestPath("McGill", "Viau"));
+            ShortestPathUtil.getNewInstanceForUnitTest().getShortestPath("McGill", "Viau"));
     }
 
   @Test
   public void getShortestPathInvalidStartingStationShouldThrowStationsNotValidException() {
     assertThrows(StationsNotValidException.class, () ->
-        ShortestPathUtil.getInstance().getShortestPath("MMcGill", "Viau"));
+        ShortestPathUtil.getNewInstanceForUnitTest().getShortestPath("MMcGill", "Viau"));
   }
 
   @Test
   public void getShortestPathInvalidDestinationStationShouldThrowStationsNotValidException() {
     assertThrows(StationsNotValidException.class, () ->
-        ShortestPathUtil.getInstance().getShortestPath("McGill", "VViau"));
+        ShortestPathUtil.getNewInstanceForUnitTest().getShortestPath("McGill", "VViau"));
   }
 
   @Test
   public void getShortestPathInvalidStationsShouldThrowStationsNotValidException() {
     assertThrows(StationsNotValidException.class, () ->
-        ShortestPathUtil.getInstance().getShortestPath("MMcGill", "VViau"));
+        ShortestPathUtil.getNewInstanceForUnitTest().getShortestPath("MMcGill", "VViau"));
   }
 }
