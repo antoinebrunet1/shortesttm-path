78d98861724e3ef2a4478b3f76c8dd4b3408c4d4
Antoine Brunet <antoinebrunet1@outlook.com>
2026-08-16 18:14:03 -0400

    Used Autowired for stationRepository in ShortestPathService

--- backend/shortesttm_path/src/main/java/com/example/shortesttmpath/service/ShortestPathService.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttmpath/service/ShortestPathService.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,7 +22,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShortestPathService {
-  private final StationRepository stationRepository = new StationRepository();
+  @Autowired
+  private StationRepository stationRepository;
   private final List<List<Edge>> graph = new GraphUtil(stationRepository).getGraph();
 
   public ShortestPathService() throws IOException {
