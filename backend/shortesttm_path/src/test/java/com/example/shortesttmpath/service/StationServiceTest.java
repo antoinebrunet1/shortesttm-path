14b2b0816217d6c769933983203a53a330bf7258
Antoine Brunet <antoinebrunet1@outlook.com>
2026-08-17 02:16:32 -0400

    Fixed StationServiceTest

--- backend/shortesttm_path/src/test/java/com/example/shortesttmpath/service/StationServiceTest.java
+++ backend/shortesttm_path/src/test/java/com/example/shortesttmpath/service/StationServiceTest.java
@@ -6,9 +6,12 @@ import static org.mockito.Mockito.when;
 import com.example.shortesttmpath.repository.StationRepository;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class StationServiceTest {
   @Mock
   StationRepository stationRepository;
