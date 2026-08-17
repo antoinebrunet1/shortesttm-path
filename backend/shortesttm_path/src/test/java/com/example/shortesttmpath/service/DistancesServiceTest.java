b1b6b0a21312f9b9283f364822014c510e52ef0d
Antoine Brunet <antoinebrunet1@outlook.com>
2026-08-17 02:06:12 -0400

    Fixed DistancesServiceTest

--- backend/shortesttm_path/src/test/java/com/example/shortesttmpath/service/DistancesServiceTest.java
+++ backend/shortesttm_path/src/test/java/com/example/shortesttmpath/service/DistancesServiceTest.java
@@ -8,9 +8,12 @@ import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class DistancesServiceTest {
   @Mock
   FileService fileService;
