bb8f3c8d9bd05e74c796d8c6b477be6f1bf747c2
Antoine Brunet <antoinebrunet1@outlook.com>
2026-08-24 20:59:07 -0400

    Remove invalid station exception related content for API testing

--- api_testing/shortesttm_path_api_testing/src/test/java/controllers/Tests.java
+++ api_testing/shortesttm_path_api_testing/src/test/java/controllers/Tests.java
@@ -82,32 +82,4 @@ public class Tests {
 
     Assert.assertEquals(bodyAsString, expectedBodyAsString);
   }
-
-  private void getShortestPathAtLeastOneInvalidStation(String startingStation,
-                                                       String destinationStation) {
-    Response response = getResponseForShortestPath(startingStation, destinationStation);
-    int statusCode = response.getStatusCode();
-
-    Assert.assertEquals(statusCode, 400);
-
-    String bodyAsString = response.getBody().asString();
-    String expectedBodyAsString = "Provided stations are not valid";
-
-    Assert.assertEquals(bodyAsString, expectedBodyAsString);
-  }
-
-  @Test
-  public void getShortestPathInvalidStartingStation() {
-    getShortestPathAtLeastOneInvalidStation("MMcGill", "Viau");
-  }
-
-  @Test
-  public void getShortestPathInvalidDestinationStation() {
-    getShortestPathAtLeastOneInvalidStation("McGill", "VViau");
-  }
-
-  @Test
-  public void getShortestPathInvalidStations() {
-    getShortestPathAtLeastOneInvalidStation("MMcGill", "VViau");
-  }
 }
