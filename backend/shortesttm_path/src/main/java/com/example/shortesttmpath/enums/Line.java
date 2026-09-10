9d7f8171313cccd00de63c8cc2a849902dd242cb
Antoine Brunet <antoinebrunet1@outlook.com>
2026-09-10 18:53:06 -0400

    Removed Javadoc comments for the attributes of the Line Enum

--- backend/shortesttm_path/src/main/java/com/example/shortesttmpath/enums/Line.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttmpath/enums/Line.java
@@ -4,20 +4,8 @@ package com.example.shortesttmpath.enums;
  * Represents a metro line.
  */
 public enum Line {
-  /**
-   * The blue line.
-   */
   BLUE,
-  /**
-   * The green line.
-   */
   GREEN,
-  /**
-   * The orange line.
-   */
   ORANGE,
-  /**
-   * The yellow line.
-   */
   YELLOW
 }
