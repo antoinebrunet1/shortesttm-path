aef6c68d97023350b1ebaa1c3bd2f82b73f10121
Antoine Brunet <antoinebrunet1@outlook.com>
2026-09-11 21:58:13 -0400

    Used @EqualsAndHashCode instead of overriding equals in NonEndingStationInPathBean

--- backend/shortesttm_path/src/main/java/com/example/shortesttmpath/data/NonEndingStationInPathBean.java
+++ backend/shortesttm_path/src/main/java/com/example/shortesttmpath/data/NonEndingStationInPathBean.java
@@ -2,15 +2,16 @@ package com.example.shortesttmpath.data;
 
 import com.example.shortesttmpath.enums.Line;
 import com.example.shortesttmpath.enums.Station;
-import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * Represents any station in a shortest path that is not the destination station.
  */
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode
 public class NonEndingStationInPathBean {
   Station name;
   Line line;
@@ -20,14 +21,5 @@ public class NonEndingStationInPathBean {
    * The default constructor.
    */
   public NonEndingStationInPathBean() {
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (!(o instanceof NonEndingStationInPathBean that)) {
-      return false;
-    }
-    return Objects.equals(name, that.name) && Objects.equals(line, that.line)
-        && Objects.equals(direction, that.direction);
   }
 }
